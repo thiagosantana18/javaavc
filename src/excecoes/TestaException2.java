3e4209968ab8ce9e4e98cf311e2b49a8aaaaf93f
Thiago <thigoncalves64@gmail.com>
2020-09-30 11:53:32 -0300

    Commit Uptade 30/09/2020

--- src/excecoes/TestaException2.java
+++ src/excecoes/TestaException2.java
@@ -1,10 +1,10 @@
 package excecoes;
 
 import java.io.FileNotFoundException;
-import java.sql.SQLException;
 
 public class TestaException2 {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		try {
 			new java.io.FileReader("arquivoDeTeste.txt");
